d5147e5ccc9eee894b1eb0c6b7c0d1eed764f50f
luke <lucas.steven.nethercott+github@gmail.com>
2025-02-13 10:29:27 -0700

    ts type clean up

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient('https://lbfcegnaffgqvbllwimf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiZmNlZ25hZmZncXZibGx3aW1mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk0MjMyMzMsImV4cCI6MjA1NDk5OTIzM30.71ztpxWbFxUJwyGxNv451treAOI8NkPeREwSLhmNHyc')
 
+type Jellybean = { id: string; flavor: string }
+
 function App() {
-  const [jellybeans, setJellybeans] = useState([])
+  const [jellybeans, setJellybeans] = useState<Jellybean[]>([])
 
   useEffect(() => {
     getJellybeans()
   }, [])
 
   async function getJellybeans() {
-    const { data } = await supabase.from('jellybeans').select()
-    setJellybeans(data)
+    const { data }: { data: Jellybean[] | null } = await supabase.from('jellybeans').select()
+    setJellybeans(data ?? [])
   }
 
   return (
